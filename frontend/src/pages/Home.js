6e7a1d9cfd048c187fb0ce5f631364f39633820d
Bruno Silva <lealbruno759@gmail.com>
2025-04-02 16:51:19 -0300

    Adicionando frontend

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem-vindo ao Sistema de Franquias</h1>
+            <p>Escolha uma opção no menu para começar.</p>
+        </div>
+    );
+};
+
+export default Home;
